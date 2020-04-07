08ea484a9acb3db86fed0c2b39510e599f8874e6
Jung Lee <lee.jung.s95@gmail.com>
2020-04-07 02:39:36 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
+rainbow_colors = ["R: red", "O: orange", "Y: yellow", "G:green", "B: blue", "I: indigo", "V: violet"]
 def #display_rainbow(color)
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
